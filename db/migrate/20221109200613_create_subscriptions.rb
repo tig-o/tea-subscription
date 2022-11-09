b017512785eb5be448b3af0c887e7a05874f68df
Thiago Silveira <thiago1992@yahoo.com>
2022-11-09 13:08:13 -0700

    Fix: CreateSubcriptions to CreateSubscriptions to resolve model spec errors

--- db/migrate/20221109200613_create_subscriptions.rb
+++ db/migrate/20221109200613_create_subscriptions.rb
@@ -1,9 +1,9 @@
-class CreateSubcriptions < ActiveRecord::Migration[5.2]
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
-    create_table :subcriptions do |t|
+    create_table :subscriptions do |t|
       t.string :title
       t.float :price
-      t.integer :status, default: 0
+      t.integer :status
       t.integer :frequency
       t.references :customer, foreign_key: true
       t.references :tea, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_180230) do
+ActiveRecord::Schema.define(version: 2022_11_09_200613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2022_11_09_180230) do
     t.index ["tea_id"], name: "index_subcriptions_on_tea_id"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.integer "status"
+    t.integer "frequency"
+    t.bigint "customer_id"
+    t.bigint "tea_id"
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -42,4 +53,6 @@ ActiveRecord::Schema.define(version: 2022_11_09_180230) do
 
   add_foreign_key "subcriptions", "customers"
   add_foreign_key "subcriptions", "teas"
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
