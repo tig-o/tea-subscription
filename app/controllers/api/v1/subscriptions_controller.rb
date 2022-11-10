3ab82666dcb5b7c446f48a4bcafae29313423cdd
Thiago Silveira <thiago1992@yahoo.com>
2022-11-09 21:40:32 -0700

    Feat: Create subscription sad path, no tea or customer provided

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::SubscriptionsController < ApplicationController
     subscription = Subscription.create(subscription_params)
     if subscription.save
       render json: SubscriptionSerializer.new(subscription), status: 201
+    else
+      render json: {error: subscription.errors.full_messages.to_sentence}, status: 400
     end
   end
 
