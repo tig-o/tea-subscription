33a60f833484eed1079734221286729079ee1bee
Thiago Silveira <thiago1992@yahoo.com>
2022-11-09 22:58:56 -0700

    Add: Cancel sub route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/customers/:id/subscriptions", to: "subscriptions#create"
+      patch "/customers/:customer_id/subscriptions/:subscription_id", to: "subscriptions#update"
     end
   end
 end
